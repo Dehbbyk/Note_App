b5bddb96ea21c6db1d6b92be0e39c6a795177278
HP <deborah.oyinda@yahoo.com>
2023-12-05 20:57:27 +0100

    My first compose app

---
+++ app/src/main/java/com/debbie/noteapp/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.debbie.noteapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.semantics.Role.Companion.Button
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.debbie.noteapp.ui.theme.NoteAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            NoteAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Column {
+                        Text(
+                            text = "Hello, Welcome to my App!",
+                            fontSize = 20.sp,
+                            textAlign = TextAlign.Center
+                        )
+                        Button(onClick = {
+
+                        }){
+                            Text(text = "Login")
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
