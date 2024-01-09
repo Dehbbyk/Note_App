9a70188948ceada8ef4662500d2249304d5f260a
HP <deborah.oyinda@yahoo.com>
2024-01-09 19:49:05 +0100

    Adjustment made on the padding

--- app/src/main/java/com/debbie/noteapp/MainActivity.kt
+++ app/src/main/java/com/debbie/noteapp/MainActivity.kt
@@ -5,20 +5,31 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.semantics.Role.Companion.Button
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.debbie.noteapp.ui.theme.NoteAppTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -28,20 +39,43 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Column {
-                        Text(
-                            text = "Hello, Welcome to my App!",
-                            fontSize = 20.sp,
-                            textAlign = TextAlign.Center
-                        )
-                        Button(onClick = {
-
-                        }){
-                            Text(text = "Register")
-                        }
+                    LoginScreen()
                     }
                 }
             }
         }
     }
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun LoginScreen(){
+    var username by remember { mutableStateOf("")}
+    var password by remember { mutableStateOf("")}
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.padding(all = 8.dp)){
+            Text(
+                text = "Simple Login page!",
+                fontSize = 20.sp,
+                textAlign = TextAlign.Center
+            )
+            TextField(
+                value = username,
+                onValueChange = {usernameInput -> username = usernameInput},
+                label = { Text("Username")},
+                modifier =  Modifier.fillMaxWidth().padding(bottom = 10.dp)
+            )
+            TextField(
+                value = password,
+                onValueChange = {pwdInput -> password = pwdInput},
+                label = {Text("Password")},
+                visualTransformation = PasswordVisualTransformation(),
+                modifier =  Modifier.fillMaxWidth().padding(bottom = 10.dp)
+            )
+            Button(onClick = {}){
+                Text(text = "Login")
+            }
+    }
+
 }
